8eca318d8b4bee69a5901396185ec9650b695eb2
Yash Dhadod <yashdhadod@gmail.com>
2025-12-02 23:05:27 +0530

    CORS

--- server/server.js
+++ server/server.js
@@ -1,36 +1,43 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import orderRoutes from "./routes/order.route.js";
-// import paymentRoutes from "./routes/payment.route.js";
-// import analyticsRoutes from "./routes/analytics.route.js";
-
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 const __dirname = path.resolve();
 
 // middleware
-app.use(express.json({ limit: "10mb" })); // allows express formatting: JSON data.
-app.use(cookieParser()); // allows express formatting: Cookies.
+app.set("trust proxy", 1);
+
+app.use(
+  cors({
+    origin: [
+      "https://humbas-nw-1.onrender.com",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
 
 // Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/product", productRoutes);
-app.use("/api/v1/cart", cartRoutes); 
+app.use("/api/v1/cart", cartRoutes);
 app.use("/api/v1/order", orderRoutes);
 
-// app.use("/api/v1/payments", paymentRoutes);
-// app.use("/api/v1/analytics", analyticsRoutes);
-
+// Production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/dist")));
 
@@ -38,8 +45,8 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
-// endpoints...
+
 app.listen(PORT, () => {
-  console.log("Server has started successfully!!! at http://localhost:" + PORT);
+  console.log("Server started on " + PORT);
   connectDB();
 });
